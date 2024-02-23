2208f7db0886f0bd2fa8cfdec8cc26afd5a86015
jumin20099 <jumin20099@gmail.com>
2024-02-24 06:19:04 +0900

    글쓴이에게만 게시글 수정 버튼 보이기

--- pages/posts.php
+++ pages/posts.php
@@ -17,6 +17,8 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $stmt->execute();
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+            $postUser_idx = isset($post["user_idx"]) ? $post["user_idx"] : '';
+
             $divInnerText = "";
             $divInnerText .= "<div class='post'>";
             $divInnerText .= "<h2 class='title'>" . $post["title"] . "</h2>";
@@ -24,6 +26,9 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $divInnerText .= "<p class='id'> 게시글 ID : " . $post["post_idx"] . "</p>";
             $divInnerText .= "<p class='id'> 작성일 : " . $post["write_date"] . "</p>";
             $divInnerText .= "<p class='content'>" . $post["content"] . "</p>";
+            if($_SESSION['user_idx'] == $postUser_idx){
+                $divInnerText .= "<button><a href='edit' style=color:black>수정하기</a></button>";
+            }
             $divInnerText .= "</div>";
             $divInnerText .= "<br>";
             echo $divInnerText;
@@ -31,3 +36,15 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     ?>
 </div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="edit"></a>
+</body>
+</html>
